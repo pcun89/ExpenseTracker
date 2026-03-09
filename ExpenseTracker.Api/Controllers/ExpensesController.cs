40f5ac0f6e24d8ed8461941925a98027fe1304e1
pcun89 <phillippcun89@gmail.com>
2026-03-09 15:44:00 -0500

    update1.8

--- ExpenseTracker.Api/Controllers/ExpensesController.cs
+++ ExpenseTracker.Api/Controllers/ExpensesController.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
+[AllowAnonymous]
 [ApiController]
 [Route("api/[controller]")]
-[AllowAnonymous]  
 public class ExpensesController : ControllerBase
 {
     private static readonly List<object> Expenses = new();
